2ee1356d54b877fd4e4453510c1768697323e21a
Slakkan <lnpastinante@gmail.com>
2022-05-20 15:11:20 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+## Welcome to my Portfolio repository
+
+- To run the project locally use "npm run dev"
+
+**have any suggestions? [send me an email](mailto:lnpastinante@gmail.com?subject=Portfolio)**
---
+++ pages/index.tsx
@@ -0,0 +1,12 @@
+import type { NextPage } from 'next'
+import styles from '../styles/Home.module.scss'
+
+const Home: NextPage = () => {
+  return (
+    <div className={styles.title}>
+        <h1>Initial Commit</h1>
+    </div>
+  )
+}
+
+export default Home
